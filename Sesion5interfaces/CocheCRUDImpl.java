b6b022352aab7fa5e190a416bd5883daccc54ddf
ivanerios <93769784+ivanerios@users.noreply.github.com>
2021-11-05 12:27:00 +0100

    Add files via upload

---
+++ Sesion5interfaces/CocheCRUDImpl.java
@@ -0,0 +1,26 @@
+package Sesion5interfaces;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CocheCRUDImpl implements CocheCRUD {
+
+    private List<Coche> coches = new ArrayList<>();
+
+    @Override
+    public void save(Coche coche) { coches.add(coche);
+
+    }
+
+    @Override
+    public List<Coche> findAll() {
+        return coches;
+    }
+
+    @Override
+    public void delete(Coche coche) {
+
+        System.out.println(coche.marca + " " + coche.modelo);
+
+    }
+}
